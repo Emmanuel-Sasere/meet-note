b8366a7fe9087157b68f7bd8358dbbb20a103378
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2025-08-31 06:15:22 +0100

    Chore: Updated main.go

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+
+f	"fmt"
+	"os"
+)
+
+func main () {
+	if len(os.Args) < 2 {
+		f.Println("Usage: go run main.go [add | list | delete] [note]")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+			f.Println("Please provide a note to add.")
+			return
+		}
+
+		note := os.Args[2]
+		err := AddNotes(note)
+		if err != nil {
+			f.Println ("Error adding note: %w", err)
+		}else{
+			f.Println("Note added successfully.", note)
+		}
+
+	case "list":
+		
+
+	}
+		
+}
---
+++ notes.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	f "fmt"
+	"os"
+)
+
+
+
+
+
+type Note struct {
+	Text string  `json:"text"`
+}
+
+
+func SaveNotes(notes []Note) error {
+	data, err := json.MarshalIndent(notes, "" , " ")
+	if err != nil {
+		f.Println("fail to marshal notes: %w", err)
+	}
+	return os.WriteFile("notes.json", data, 0644)
+}
+
+func LoadNotes() ([]Note, error) {
+	data, err := os.ReadFile("notes.json")
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Note{}, nil
+		}
+		return nil, err
+	}
+	var notes []Note
+	err = json.Unmarshal(data, &notes)
+	if err != nil {
+		f.Println("fail to unmarshal notes: %w", err)
+		
+	}
+	return notes, nil
+}
+
+func AddNotes(text string) error {
+	notes, err := LoadNotes()
+	if err != nil {
+		return err
+	}
+	notes = append(notes, Note{Text: text})
+	return SaveNotes(notes)
+}
+
+func deleteNote(index int) error {
+	notes, err := LoadNotes()
+	if err != nil {
+		return err
+	}
+	if index < 0 || index>= len(notes) {
+		return f.Errorf("invalid note index:")
+	}
+	notes = append(notes[:index], notes[index+1:] ...)
+	return SaveNotes(notes)
+}
